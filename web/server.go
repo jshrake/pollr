c9249fd15af09857bfc30d27e57bfaa96c5089b9
jshrake <justinshrake@gmail.com>
2014-05-06 19:53:16 -0700

    gzip web assets

--- web/server.go
+++ web/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/gzip"
 )
 
 var addr = flag.String("addr", "localhost:8080", "server address")
@@ -13,6 +14,7 @@ var addr = flag.String("addr", "localhost:8080", "server address")
 func main() {
 	flag.Parse()
 	m := martini.Classic()
+	m.Use(gzip.All())
 	m.Use(martini.Static("public/src"))
 	m.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "public/src/index.html")
